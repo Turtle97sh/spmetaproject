f9aae40219d54d71bdb5b885cc2b0d81feb85f9c
Turtle97sh <hyeok8554@gmail.com>
2025-07-28 15:09:21 +0900

    미니게임실행불가. 수정중

--- Assets/MiniGamestart.cs
+++ Assets/MiniGamestart.cs
@@ -3,38 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameStart : MonoBehaviour
+public class MiniGamestart : MonoBehaviour
 {
-    private bool isPlayerNear = false;
-
-    void Update()
-    {
-        // 플레이어가 상호작용 범위 내에 있고, E키를 눌렀을 때 씬 전환
-        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
-        {
-            Debug.Log("E pressed, loading MiniGameScene...");
-            // 씬을 단독으로 로드해서 기존 씬 닫기
-            SceneManager.LoadScene("MiniGameScene", LoadSceneMode.Single);
-        }
-    }
-
-    // 플레이어가 트리거 영역에 들어왔을 때
-    void OnTriggerEnter2D(Collider2D other)
+    // 플레이어가 트리거에 들어오면 즉시 씬 이동
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            isPlayerNear = true;
-            Debug.Log("Player entered trigger area.");
-        }
-    }
-
-    // 플레이어가 트리거 영역에서 나갔을 때
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerNear = false;
-            Debug.Log("Player exited trigger area.");
+            Debug.Log("플레이어가 미니게임 오브젝트에 닿음 → 씬 전환");
+            SceneManager.LoadScene("MiniGameScene");
         }
     }
 }
